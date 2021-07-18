54cf5c9ba690835d75f00811f3e77e3bd99047f8
ressKim <sh21dlat@gmail.com>
2021-07-18 21:35:17 +0900

    servlet exception - interceptor 처리 study

--- src/main/java/com/study/exception/WebConfig.java
+++ src/main/java/com/study/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package com.study.exception;
 
 import com.study.exception.filter.LogFilter;
+import com.study.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,8 +14,17 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean logFilter(){
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry
+                .addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");//오류 페이지 경로
+    }
+
+    //    @Bean
+    public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
         filterRegistrationBean.setOrder(1);
